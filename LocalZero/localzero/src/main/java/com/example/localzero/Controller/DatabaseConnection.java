6f6a90c9b0f57f3dee8a94e8f8c549e1a6a878da
khanhhuy123 <khanhhuy7591@gmail.com>
2025-04-29 12:35:52 +0200

    intellj_change

--- LocalZero/localzero/src/main/java/com/example/localzero/Controller/DatabaseConnection.java
+++ LocalZero/localzero/src/main/java/com/example/localzero/Controller/DatabaseConnection.java
@@ -18,7 +18,7 @@ public class DatabaseConnection {
         try {
             dotenv = Dotenv.configure()
         .directory(System.getProperty("user.dir"))
-        .filename("LocalZero/localzero/.env")
+        .filename(".env")
         .load();
 
             String databasename = getDatabaseName();
